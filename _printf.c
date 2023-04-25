49b7dd9ca79ab571077a380d0885f1a18e3b7973
theenduranceA <theenduranceaneke@gmail.com>
2023-04-24 19:59:39 -0700

    specifier0.c

---
+++ _printf.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _printf - My custom printf that replicates what printf does
+ * @format: List of types
+ *
+ * Return:  the number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int count = 0;
+	print_type argument[] = {
+		{"c", _print_char},
+		{"s", _print_string},
+		{"%", _print_percent},
+		{"d", _print_int},
+		{"i", _print_int},
+		{"r", _print_reverse},
+		{"R", _print_rot13},
+		{"b", _print_binary},
+		{"u", _print_unsigned},
+		{"o", _print_octal},
+		{"x", _print_hex_l},
+		{"X", _print_hex_u},
+		{NULL, NULL}
+	};
+
+	va_start(args, format);
+	count = get_print(format, argument, ap);
+	va_end(args);
+	return (count);
+}
---
+++ main.h
@@ -0,0 +1,42 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define BUFF_SIZE 1024
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * struct printSpecifier - Function to print specifiers.
+ * @zed: The operators to be printed.
+ * @f: The function
+ */
+
+typedef struct printSpecifier
+{
+	char *zed;
+	int (*f)(va_list args);
+} print_type;
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+int _print_char(va_list args);
+int _print_string(va_list args);
+int _print_percent(va_list args);
+int _print_int(va_list args);
+int _print_decimal(va_list args);
+int _print_binary(va_list args);
+int _print_unsigned(va_list args);
+int _print_octal(va_list args);
+int _print_hex(va_list args);
+int _print_HEX(va_list args);
+int _print_STRING(va_list args);
+int _print_pointer(va_list args);
+int _print_reverse(va_list args);
+int _print_rot13(va_list args);
+char *convert(unsigned int num, int base);
+int my_printf(const char *format, print_type argument[], va_list args);
+
+#endif
